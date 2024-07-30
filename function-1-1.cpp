13ad0810a65ea72bb539d432627a61dcf81b4c94
BrandonHo0419 <brandon.ho0419@gmail.com>
2024-07-30 14:48:19 +0930

    Update function-1-1.cpp

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -7,12 +7,8 @@ void sum_diagonal(int array[4][4]) {
     int sum = 0;
 
     for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-            if(array[i][j] == i) {
-                sum = array[i][j];
-            }
+        sum +=array[i][i];
         }
-    }
 
 
     std::cout << sum << std::endl;

d1a479b31408394af51668094f1861f344e6fd07
BrandonHo0419 <brandon.ho0419@gmail.com>
2024-07-30 21:33:10 +0930

    Improvement

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -2,14 +2,11 @@
 
 
 void sum_diagonal(int array[4][4]) {
-
-    int size = 4;
     int sum = 0;
 
     for(int i = 0; i < 4; i++) {
         sum +=array[i][i];
         }
-
 
     std::cout << sum << std::endl;
 
--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,7 +3,7 @@
 extern void sum_diagonal(int array[4][4]);
 
 int main() {
-    int array[4][4] = { {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}
+    int array[4][4] = { {-1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}
     };
 
     sum_diagonal(array);

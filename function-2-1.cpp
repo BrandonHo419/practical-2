cd564782315a9464f1c5d6f88e2cc20efe72a78b
BrandonHo0419 <brandon.ho0419@gmail.com>
2024-08-01 11:33:22 +0930

    Update function-2-1.cpp

--- function-2-1.cpp
+++ function-2-1.cpp
@@ -13,7 +13,7 @@ void print_binary_str(std::string decimal_number) {
     if (convert <= 0) {
         std::cout << "ERROR" << std::endl;
     }
-    
+
     int i = 0;
     int arr[100] = {0};
     int position = 0;
@@ -21,12 +21,12 @@ void print_binary_str(std::string decimal_number) {
     while (convert > 0) {
         arr[position++] = (convert % 2);
 
-        decimal_number = convert / 2;
+        convert = convert / 2;
         i++;
     }
 
-    for (int j = i - 1; j>= 0; j--){
-        std::cout << arr[j] << " ";
+    for (int j = position - 1; j>= 0; j--){
+        std::cout << arr[j] << " " ;
     }
     
 
